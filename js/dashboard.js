eae075cde10a9a99e813ac1cc794e936e5f0a1b2
bmtz <mtzsts1@gmail.com>
2023-05-17 14:15:43 -0600

    Metodos para agregar contactos

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,38 @@
+// Definición de la clase cuenta y contacto
+class Cuenta {
+  constructor(nTarjeta, nip, saldo, id) {
+    this.nTarjeta = nTarjeta;
+    this.nip = nip;
+    this.saldo = saldo;
+    this.id = id;
+  }
+}
+class Contacto {
+  constructor(nombre, cuenta, id) {
+    this.nombre = nombre;
+    this.cuenta = cuenta;
+    this.id = id;
+  }
+}
+
+// Creación del arreglo de cuentas y de contactos
+const cuentas = [
+  new Cuenta("1234567890",3234, 90000,1),
+  new Cuenta("8476398596",5387, 15500,2),
+  new Cuenta("6239453274",3125, 45000,3),
+  new Cuenta("9086428371",2151, 3200,4),
+  new Cuenta("1927452913",6087, 16478,5),
+];
+const contactos = new Array();
+
+//Recupera el id de la persona
+const idUsuario = localStorage.getItem('personaId');
+// Recuperar la cadena JSON del localStorage
+const personasJSON = localStorage.getItem('personas');
+// Convertir la cadena JSON a un arreglo de objetos
+const personas = JSON.parse(personasJSON);
+
+
 
 //Mostrar y Ocultar la tarjeta
 const botonMostrarTarjeta = document.getElementById('mostrarTarjeta');
@@ -12,4 +47,23 @@ botonMostrarTarjeta.addEventListener('click', function() {
     tarjeta.classList.remove('oculta');
     tarjetaVisible = true;
   }
+  
+});
+
+// Crear Nuevo Contacto
+const nuevoContacto = document.getElementById('nuevoContacto');
+nuevoContacto.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar que el formulario se envíe
+  
+  const numeroCuenta = document.getElementById('CnCuenta').value;
+  const nombre = document.getElementById('Cnombre').value;
+  const contacto = {
+    nombre: nombre,
+    numeroCuenta: numeroCuenta
+  };
+  contactos.push(contacto);
+  // Restablecer el formulario
+  nuevoContacto.reset();
+
+  console.log(contactos[contactos.length - 1]);
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+// Definición de la clase Persona
+class Persona {
+    constructor(usuario, contraseña, tarjeta, id) {
+        this.usuario = usuario;
+        this.contraseña = contraseña;
+        this.tarjeta = tarjeta;
+        this.id = id;
+    }
+}
+
+// Creación del arreglo de objetos
+const personas = [
+    new Persona('root', 'root', '1234567890123456', 0),
+    new Persona('Juan', '12345', '5435734658347657', 1),
+    new Persona('Maria', 'a1b2c3', '0293472386473773', 2),
+    new Persona('Felipe', 'abcde', '9823486374637434', 3),
+    new Persona('Rosa', 'aabbccdd', '8234762736423859', 4)
+];
+// Convertir el arreglo a una cadena JSON
+const personasJSON = JSON.stringify(personas);
+
+
 document.getElementById("login").addEventListener("submit", function (event) {
     // Evita que el formulario se envíe
     event.preventDefault();
@@ -6,11 +28,18 @@ document.getElementById("login").addEventListener("submit", function (event) {
     var usuario = document.getElementById("usuario").value;
     var contraseña = document.getElementById("contrasena").value;
 
-    console.log(usuario)
-    // Rrealiza la verificación de usuario y contraseña
-    if (usuario === "root" && contraseña === "root") {
+    // Verificar el usuario y la contraseña
+    var personaEncontrada = personas.find(function (persona) {
+        return persona.usuario === usuario && persona.contraseña === contraseña;
+    });
+
+    if (personaEncontrada) {
+        // Usuario y contraseña válidos
+        localStorage.setItem('personaId', personaEncontrada.id);
+        localStorage.setItem('personas', personasJSON);
         window.location.href = "dashboard.html";
     } else {
+        // Usuario o contraseña incorrectos
         alert("Usuario o contraseña incorrectos");
     }
 });
